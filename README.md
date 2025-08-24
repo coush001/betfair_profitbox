159142fc57494df04c66dd3b8a14901c32bb8a30
coush001 <coush001@users.noreply.github.com>
2025-08-24 10:40:39 +0100

    adding data

--- README.md
+++
@@ -1,1 +0,0 @@
-# betfair_profitbox
--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print('hey world')
+import betfairlightweight
+print('hell0')
+
+trading = betfairlightweight.APIClient(
+    username="hcoussens1",
+    app_key="zLsKKtBX7Z15FuSf",
+    certs=("./certs/client-2048.crt", "./certs/client-2048.key")
+)
+trading.session_token = "your_saved_session_token"
+
